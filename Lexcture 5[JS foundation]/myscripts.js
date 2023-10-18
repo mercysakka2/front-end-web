2faee0fff80d4fa5844f4be3f43b00a30488f00b
mercysakka2 <145364944+mercysakka2@users.noreply.github.com>
2023-10-18 21:27:43 +0800

    Add files via upload

---
+++ Lexcture 5[JS foundation]/myscripts.js
@@ -0,0 +1,8 @@
+// Turn ON
+function turnon () {
+    document.getElementById('myImage').src='pic_on (1).gif';
+}
+// Turn OFF
+function turnoff (){
+    document.getElementById('myImage').src='pic_off (1).gif';
+}
