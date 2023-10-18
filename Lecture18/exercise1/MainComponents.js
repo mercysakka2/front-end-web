4ccfbbd758d34e23c0c427cffcad0cb629dbf7e8
mercysakka2 <145364944+mercysakka2@users.noreply.github.com>
2023-10-18 11:24:30 +0800

    Add files via upload

---
+++ Lecture18/exercise1/MainComponents.js
@@ -0,0 +1,32 @@
+﻿// create class message
+class UserMessage extends React.Component { 
+	render() {// render function
+		return ( // return JSX
+			<div>
+				<small>(this.props.user):</small> 
+				<p>{this.props.content}</p>
+				<hr />
+			</div>
+		);
+	}
+}
+
+// create component ChatBox
+class ChatBox extends React.Component {
+	// render function
+	render() { // return JSX
+		return (
+			<div class="main">
+				<UserMessage user="Semmy Taju" content="Hi, apa kabar semua?" />
+				<UserMessage user="Mercy Sakka" content="Baik, apa kabar juga?" />
+				<UserMessage user="Angie Shannon" content="Sehat juga." />
+				<UserMessage user="Marion Simon" content="Aku senang dan girang selalu !!!" />
+				<UserMessage user="Lovi Djarang" content="Senang bisa juga via message dengan kalian semua." /> 
+				<UserMessage user="Inka Sumahap" content="Biasa aja kale." />
+			</div>
+		);
+	}
+}
+
+// render elements to DOM
+ReactDOM.render(<ChatBox />, document.getElementById("app"));
---
+++ Lecture18/exercise2/components/Footer.js
@@ -0,0 +1,11 @@
+// create component header
+class FooterPage extends React.Component {
+	render() {
+		// return JSX
+		return(
+			<div class="footer">
+			  <h2>Universitas Klabat</h2>
+			</div>
+		);
+	}
+}
